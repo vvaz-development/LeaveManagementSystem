e979ad14686f0a638ba9b12810639a3a42b88b6a
vvaz-software-engineer <vincent.vaz.software.engineer@outlook.com>
2025-02-26 17:45:35 -0500

    Add project files.

---
+++ LeaveManagementSystem.Web/Controllers/TestController.cs
@@ -0,0 +1,19 @@
+﻿using LeaveManagementSystem.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LeaveManagementSystem.Web.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            var data = new TestViewModel
+            {
+                Name = "Vincent Vaz",
+                DateOfBirth = new DateTime(1971, 12, 29)
+            };
+
+            return View(data);
+        }
+    }
+}
